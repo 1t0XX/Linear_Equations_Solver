0c902c87aa7d33a47f2d9dd49a47350047deb8fe
1t0XX <53831070+1t0XX@users.noreply.github.com>
2022-08-15 22:52:03 +0300

    Initial commit

---
+++ Linear Equations Solver/task/src/solver/LinearEquation.java
@@ -0,0 +1,20 @@
+package solver;
+
+public class LinearEquation {
+
+    private final Matrix coefficients;
+    private final Matrix constants;
+
+    public LinearEquation(Matrix coefficients, Matrix constants) {
+        this.coefficients = coefficients;
+        this.constants = constants;
+    }
+
+    public Matrix getCoefficients() {
+        return coefficients;
+    }
+
+    public Matrix getConstants() {
+        return constants;
+    }
+}
